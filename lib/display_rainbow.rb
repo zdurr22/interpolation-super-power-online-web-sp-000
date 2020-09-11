5304d081f1612d3e6ed35074ed0db7bc67ff6d23
Zachary Durr <zdurr22@gmail.com>
2020-09-11 22:40:38 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,3 @@
-display_rainbow = ["R: " + color1, "O: " + color2, "Y: " + color3, "G: " + color4, "B: " + color5, "I: " + color6, "V: " + color7]
 color1 = "red"
 color2 = "orange"
 color3 = "yellow"
@@ -6,5 +5,7 @@ color4 = "green"
 color5 = "blue"
 color6 = "indigo"
 color7 = "violet"
+display_rainbow = ["R: " + color1, "O: " + color2, "Y: " + color3, "G: " + color4, "B: " + color5, "I: " + color6, "V: " + color7]
+
 
 display_rainbow
