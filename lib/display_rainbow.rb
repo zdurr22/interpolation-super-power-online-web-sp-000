99ff8b94361e221d3b3dc7cb7502d01219ef771f
Zachary Durr <zdurr22@gmail.com>
2020-09-11 22:41:54 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,10 +1,3 @@
-color1 = "red"
-color2 = "orange"
-color3 = "yellow"
-color4 = "green"
-color5 = "blue"
-color6 = "indigo"
-color7 = "violet"
 def display_rainbow
   puts ["R: " + color1, "O: " + color2, "Y: " + color3, "G: " + color4, "B: " + color5, "I: " + color6, "V: " + color7]
 end
